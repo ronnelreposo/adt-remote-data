46fc05e26c7096fe3ce42baec5dbd23d0f1e3469
ronnelreposo <ronnel.reposo@gmail.com>
2024-03-22 07:18:53 +0800

    Add upload Test Coverage

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/src/test/**/*.test.ts'],
+  collectCoverage: true,
+  coverageDirectory: './out',
+  coverageReporters: ['text', 'html'],
 };
